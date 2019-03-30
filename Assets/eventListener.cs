350d7b2a6fa43ffce0751600a4da11d8e1faf1b0
Lauren Ahart <lauren5a@vt.edu>
2019-03-30 16:53:24 +0000

    Initial push

---
+++ Assets/eventListener.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventListener : MonoBehaviour
+{
+    private Renderer _renderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+    
+    }
+
+    public void OnEnter()
+    {
+        _renderer.material.color = Color.red;
+    }
+
+    public void OnExit()
+    {
+        _renderer.material.color = Color.white;
+    }
+
+    public void onGrab()
+    {
+
+    }
+}
