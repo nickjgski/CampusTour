e36d7e92eb5b90dcb9fb58cfc73b0e7679abbdbb
Junwei (Carl) Yao <mmk9624@vt.edu>
2019-04-29 20:16:10 +0000

    Adding audio to all ICAT scenes. All changes wrapped in AudioObject prefab.

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip narrationClip;
+    public ulong delay;
+    public AudioSource narrationSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        narrationSource.clip = narrationClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        narrationSource.PlayDelayed(delay);
+    }
+}
